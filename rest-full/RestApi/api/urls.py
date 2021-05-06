8d2093286a499e72a5e236cc9358818df1f19467
barbod <barbodnvb@gmail.com>
2021-05-06 22:26:36 +0430

    detail view

--- rest-full/RestApi/api/urls.py
+++ rest-full/RestApi/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import BlogList
+from .views import BlogList,BlogDetail
 
 app_name = "api"
 
 urlpatterns = [
     path("list", BlogList.as_view(), name="blog_list"),
+    path("article/<int:pk>/", BlogDetail.as_view(), name="article_detail"),
 ]
 
--- rest-full/RestApi/api/views.py
+++ rest-full/RestApi/api/views.py
@@ -1,11 +1,19 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView,RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from Blog.models import Article
 from .serializers import BlogSerializer
 from django.contrib.auth.models import User
-
+from django.shortcuts import get_object_or_404
 
 class BlogList(ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = BlogSerializer
     permission_classes = [IsAuthenticated]
+
+
+class BlogDetail(RetrieveAPIView):
+    model=Article
+    serializer_class = BlogSerializer
+    def get_object(self):
+        pk=self.kwargs['pk']
+        return get_object_or_404(Article,pk=pk)
