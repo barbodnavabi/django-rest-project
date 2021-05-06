9bd67f9ea784942a09520223ef96f6d7fae59757
barbod <barbodnvb@gmail.com>
2021-05-06 21:31:23 +0430

    first commit

---
+++ rest-full/RestApi/api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from Blog.models import Article
+from django.contrib.auth.models import User
+
+
+class BlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = "__all__"
---
+++ rest-full/RestApi/api/views.py
@@ -0,0 +1,11 @@
+from rest_framework.generics import ListCreateAPIView,
+from rest_framework.permissions import IsAuthenticated
+from Blog.models import Article
+from .serializers import BlogSerializer
+from django.contrib.auth.models import User
+
+
+class BlogList(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = BlogSerializer
+    permission_classes = [IsAuthenticated]
